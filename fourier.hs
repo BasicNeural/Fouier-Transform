2e1fac239c1c1b14335bc2928fa767f6ea025339
Heechun Jeon <heechin@getawair.com>
2019-08-28 00:36:51 +0900

    draw wave graph

--- fourier.hs
+++ fourier.hs
@@ -14,6 +14,7 @@ main = do
     initialDisplayMode $= [DoubleBuffered]
     initialWindowSize $= Size 520 520
     initialDisplayMode $= [ RGBAMode, DoubleBuffered, WithSamplesPerPixel 4 ]
+    windowPosition $= Position 100 100
     _window <- createWindow "Fouier"
     time <- getCurrentTime
     tick <- newIORef time
@@ -25,9 +26,28 @@ main = do
 
     keyboardMouseCallback $= Just (keyboardProc fDir)
     displayCallback $= display points
-    idleCallback $= Just (idle display tick frame time f fDir points)
+    idleCallback $= Just (idle _window tick frame time f fDir points)
+
+    windowPosition $= Position 600 0
+    _window2 <- createWindow "Wave"
+    keyboardMouseCallback $= Just (keyboardProc fDir)
+    displayCallback $= displayWave
+    postRedisplay $ Just _window2
 
     mainLoop
+
+displayWave = do
+    clear [ColorBuffer]
+
+    renderPrimitive Lines $ do
+        vertex $ Vertex2 (0.0) (-1.0 :: Double)
+        vertex $ Vertex2 (0.0) (1.0 :: Double)
+        vertex $ Vertex2 (-1.0) (0.0 :: Double)
+        vertex $ Vertex2 (1.0) (0.0 :: Double)
+    renderPrimitive LineStrip $ 
+        mapM_ (\x -> vertex $ Vertex2 (x / 100) (g (x / 5))) . take 201 $ iterate (+1) (-100 :: Double)
+
+    swapBuffers
 
 display :: IORef [(Double, Double)] -> IO ()
 display pointsRef = do 
@@ -42,7 +62,7 @@ display pointsRef = do
     
     swapBuffers
 
-idle display tickRef frameRef timeRef fRef fDirRef pointsRef = do
+idle window tickRef frameRef timeRef fRef fDirRef pointsRef = do
     tick <- readIORef tickRef
     curr <- getCurrentTime
     
@@ -63,7 +83,7 @@ idle display tickRef frameRef timeRef fRef fDirRef pointsRef = do
         let t = realToFrac time
         let newPoints = map (\t -> let r :+ i = (g t) * exp (-2 * pi * (0 :+ 1) * t * f) in (r, i)) . take 2000 $ iterate ((+) 0.0167) t
         writeIORef pointsRef newPoints
-        display pointsRef
+        postRedisplay $ Just window
     else
         return ()
 
