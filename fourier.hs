6d2dd641bf9ad1650499a8b1d5ff73548323438d
Heechun Jeon <heechin@getawair.com>
2019-08-27 21:59:27 +0900

    can modify f value

--- fourier.hs
+++ fourier.hs
@@ -5,6 +5,9 @@ import Data.IORef
 import Data.Time
 import Data.Complex
 
+
+g t = ((sin t + 1) / 2 + (sin (2 * t) + 1) / 2) / 2
+
 main :: IO ()
 main = do
     (_progName, _args) <- getArgsAndInitialize
@@ -16,14 +19,17 @@ main = do
     tick <- newIORef time
     frame <- newIORef 0.0
     time <- newIORef 0.0
+    f <- newIORef $ 1 / pi
+    fDir <- newIORef 0
     points <- newIORef ([] :: [(Double, Double)])
 
-    keyboardMouseCallback $= Just keyboardProc
+    keyboardMouseCallback $= Just (keyboardProc fDir)
     displayCallback $= display points
-    idleCallback $= Just (idle display tick frame time points)
+    idleCallback $= Just (idle display tick frame time f fDir points)
 
     mainLoop
- 
+
+display :: IORef [(Double, Double)] -> IO ()
 display pointsRef = do 
     clear [ColorBuffer]
     lineWidth $= 2
@@ -31,12 +37,12 @@ display pointsRef = do
     points <- readIORef pointsRef
 
     renderPrimitive LineStrip $ do
-        vertex $ Vertex2 0.0 (0.0 :: Double)
         mapM_ (\(x, y) -> vertex $ Vertex2 y x) points
+        vertex $ Vertex2 0.0 (0.0 :: Double)
     
     swapBuffers
 
-idle display tickRef frameRef timeRef pointsRef = do
+idle display tickRef frameRef timeRef fRef fDirRef pointsRef = do
     tick <- readIORef tickRef
     curr <- getCurrentTime
     
@@ -51,17 +57,22 @@ idle display tickRef frameRef timeRef pointsRef = do
 
     if frame > 0.0167 then do
         modifyIORef' frameRef ((-) 0.0167)
-        let fracTime = realToFrac time
-        points <- readIORef pointsRef
-        let g t = (cos t + 1) / 2
-        let f = 1
-        let r :+ i = (g fracTime) * exp (-2 * pi * (0 :+ 1) * fracTime * f)
-        writeIORef pointsRef $ take 1000 $ (r, i) : points
+        fDir <- readIORef fDirRef
+        modifyIORef fRef (+ 0.0005 * fDir)
+        f <- readIORef fRef
+        let t = realToFrac time
+        let newPoints = map (\t -> let r :+ i = (g t) * exp (-2 * pi * (0 :+ 1) * t * f) in (r, i)) . take 2000 $ iterate ((+) 0.0167) t
+        writeIORef pointsRef newPoints
         display pointsRef
     else
         return ()
-    
 
-keyboardProc ch state _ _
+keyboardProc fDirRef ch state _ _
     | ch == Char 'q' = exitWith ExitSuccess
+    | ch == Char 'a' && state == Down = do 
+        writeIORef fDirRef (-1)
+    | ch == Char 's' && state == Down = do 
+        writeIORef fDirRef 0
+    | ch == Char 'd' && state == Down = do 
+        writeIORef fDirRef 1
     | otherwise      = return ()
