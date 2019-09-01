0122b8b1469e762d202a5eb8bd7e71d46335b40b
Heechun Jeon <heechin@getawair.com>
2019-09-01 10:05:28 +0900

    add type annotation

--- fourier.hs
+++ fourier.hs
@@ -1,12 +1,13 @@
 import Graphics.UI.GLUT
 import Data.List
-import System.Exit
-import Data.IORef
 import Data.Time
 import Data.Complex
+import Data.IORef
+import Control.Monad
+import System.Exit
 
-
-g t = ((sin t + 1) / 2 + (sin (2 * t) + 1) / 2) / 2
+g t = sin t
+gg t = - cos t
 
 main :: IO ()
 main = do
@@ -44,55 +45,65 @@ displayWave = do
         vertex $ Vertex2 (0.0) (1.0 :: Double)
         vertex $ Vertex2 (-1.0) (0.0 :: Double)
         vertex $ Vertex2 (1.0) (0.0 :: Double)
-    renderPrimitive LineStrip $ 
+    renderPrimitive LineStrip $
         mapM_ (\x -> vertex $ Vertex2 (x / 100) (g (x / 5))) . take 201 $ iterate (+1) (-100 :: Double)
 
     swapBuffers
 
 display :: IORef [(Double, Double)] -> IO ()
-display pointsRef = do 
+display pointsRef = do
     clear [ColorBuffer]
     lineWidth $= 2
-    
+
     points <- readIORef pointsRef
 
     renderPrimitive LineStrip $ do
         mapM_ (\(x, y) -> vertex $ Vertex2 y x) points
         vertex $ Vertex2 0.0 (0.0 :: Double)
-    
+
+    let len = genericLength points
+    renderPrimitive Points $
+        vertex $ Vertex2 ((/len) . sum . map snd $ points) ((/len) . sum . map fst $ points)
+
     swapBuffers
 
+idle :: RealFloat a =>
+          Window
+          -> IORef UTCTime
+          -> IORef NominalDiffTime
+          -> IORef NominalDiffTime
+          -> IORef a
+          -> IORef a
+          -> IORef [(a, a)]
+          -> IO ()
 idle window tickRef frameRef timeRef fRef fDirRef pointsRef = do
     tick <- readIORef tickRef
     curr <- getCurrentTime
-    
+
     let diff = diffUTCTime curr tick
 
-    writeIORef tickRef (curr)
+    writeIORef tickRef curr
     modifyIORef frameRef (+diff)
     modifyIORef timeRef (+diff)
-    
+
     time <- readIORef timeRef
     frame <- readIORef frameRef
 
-    if frame > 0.0167 then do
+    when (frame > 0.0167) $ do
         modifyIORef' frameRef ((-) 0.0167)
         fDir <- readIORef fDirRef
         modifyIORef fRef (+ 0.0005 * fDir)
         f <- readIORef fRef
         let t = realToFrac time
-        let newPoints = map (\t -> let r :+ i = (g t) * exp (-2 * pi * (0 :+ 1) * t * f) in (r, i)) . take 2000 $ iterate ((+) 0.0167) t
+        let newPoints = map (\t -> let r :+ i = g (t:+0) * exp (-2 * pi * (0:+1) * (t:+0) * (f:+0)) in (r, i)) . take 2000 $ iterate ((+) 0.0167) t
         writeIORef pointsRef newPoints
         postRedisplay $ Just window
-    else
-        return ()
 
+keyboardProc :: Num a =>
+                  IORef a -> Key -> KeyState -> p1 -> p2 -> IO ()
 keyboardProc fDirRef ch state _ _
-    | ch == Char 'q' = exitWith ExitSuccess
-    | ch == Char 'a' && state == Down = do 
-        writeIORef fDirRef (-1)
-    | ch == Char 's' && state == Down = do 
-        writeIORef fDirRef 0
-    | ch == Char 'd' && state == Down = do 
-        writeIORef fDirRef 1
+    | ch == Char 'q' = exitSuccess
+    | ch == Char 'a' && state == Down = writeIORef fDirRef (-1)
+    | ch == Char 's' && state == Down = writeIORef fDirRef 0
+    | ch == Char 'd' && state == Down = writeIORef fDirRef 1
     | otherwise      = return ()
